f9c332e2ee871a6fcab900ea8f45a21760c221fe
matiaszo <tiaszuniga@gmail.com>
2024-09-13 17:06:25 -0300

    exercicio 13 pronto

---
+++ aulapratica24_ev1.py
@@ -0,0 +1,12 @@
+salario = float(input("Digite o salario: "))
+
+aumento = salario * 0.15
+aumento += salario
+
+desconto = aumento* 0.08
+desconto = aumento - desconto
+
+
+print(f"Salario inicial: {salario:5}")
+print(f"Salario com aumento: {aumento:5}")
+print(f"Salario com desconto: {desconto:5}")
