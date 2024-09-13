2e1e6d62a76335beaff313f53138994aaa4a1bf1
matiaszo <tiaszuniga@gmail.com>
2024-09-13 17:20:36 -0300

    exercicio 15 pronto

---
+++ aulapratica23_ev1.py
@@ -0,0 +1,7 @@
+valor = float(input("Digite o valor que vai abastecer: "))
+litro = float(input("Digite o valor do litro: "))
+
+
+total = valor / litro
+
+print(f"Voce vai abastecer {total:3} litros")
